deb21bf5e0afd2b2fa775fbd495f25e37c9daa04
Matej1910 <matejderonja@hotmail.com>
2026-05-18 15:46:54 +0200

    Exercise 3 completed

--- server/files/index.js
+++ server/files/index.js
@@ -1,50 +1,107 @@
-import { ElementBuilder, ParentChildBuilder } from "./builders.js";
+import { ElementBuilder, ParentChildBuilder } from './builders.js';
 
 class ParagraphBuilder extends ParentChildBuilder {
   constructor() {
-    super("p", "span");
+    super('p', 'span');
   }
 }
 
 class ListBuilder extends ParentChildBuilder {
   constructor() {
-    super("ul", "li");
+    super('ul', 'li');
   }
 }
 
 function formatRuntime(runtime) {
-  const hours = Math.trunc(runtime / 60);
-  const minutes = runtime % 60;
-  return hours + "h " + minutes + "m";
+  const hours = Math.floor(runtime / 60)
+  const minutes = runtime % 60
+  return hours + 'h ' + minutes + 'm'
 }
 
 function appendMovie(movie, element) {
-  new ElementBuilder("article").id(movie.imdbID)
-          .append(new ElementBuilder("img").with("src", movie.Poster))
-          .append(new ElementBuilder("h1").text(movie.Title))
-          .append(new ElementBuilder("p")
-              .append(new ElementBuilder("button").text("Edit")
-                    .listener("click", () => location.href = "edit.html?imdbID=" + movie.imdbID)))
-          .append(new ParagraphBuilder().items(
-              "Runtime " + formatRuntime(movie.Runtime),
-              "\u2022",
-              "Released on " +
-                new Date(movie.Released).toLocaleDateString("en-US")))
-          .append(new ParagraphBuilder().childClass("genre").items(movie.Genres))
-          .append(new ElementBuilder("p").text(movie.Plot))
-          .append(new ElementBuilder("h2").pluralizedText("Director", movie.Directors))
-          .append(new ListBuilder().items(movie.Directors))
-          .append(new ElementBuilder("h2").pluralizedText("Writer", movie.Writers))
-          .append(new ListBuilder().items(movie.Writers))
-          .append(new ElementBuilder("h2").pluralizedText("Actor", movie.Actors))
-          .append(new ListBuilder().items(movie.Actors))
-          .appendTo(element);
+  const article = document.createElement('article')
+  article.id = movie.imdbID
+
+  const img = document.createElement('img')
+  img.src = movie.Poster
+  img.alt = movie.Title + ' Poster'
+  article.append(img)
+
+  const title = document.createElement('h1')
+  title.textContent = movie.Title
+  article.append(title)
+
+  const editButton = document.createElement('button')
+  editButton.textContent = 'Edit'
+  editButton.onclick = function () {
+    location.href = 'edit.html?imdbID=' + movie.imdbID
+  }
+  article.append(editButton)
+
+  const meta = document.createElement('p')
+  meta.className = 'meta'
+  meta.textContent = 'Runtime ' + formatRuntime(movie.Runtime) + ' • Released on ' + movie.Released
+  article.append(meta)
+
+  const genreDiv = document.createElement('div')
+  genreDiv.className = 'genres'
+  movie.Genres.forEach(function (genre) {
+    const genreSpan = document.createElement('span')
+    genreSpan.className = 'genre'
+    genreSpan.textContent = genre
+    genreDiv.append(genreSpan)
+  })
+  article.append(genreDiv)
+
+  const plot = document.createElement('p')
+  plot.textContent = movie.Plot
+  article.append(plot)
+
+  const dirTitle = document.createElement('h2')
+  dirTitle.textContent = movie.Directors.length === 1 ? 'Director' : 'Directors'
+  article.append(dirTitle)
+  const dirList = document.createElement('ul')
+  movie.Directors.forEach(function (director) {
+    const li = document.createElement('li')
+    li.textContent = director
+    dirList.append(li)
+  })
+  article.append(dirList)
+
+  const writTitle = document.createElement('h2')
+  writTitle.textContent = 'Writers'
+  article.append(writTitle)
+  const writList = document.createElement('ul')
+  movie.Writers.forEach(function (writer) {
+    const li = document.createElement('li')
+    li.textContent = writer
+    writList.append(li)
+  })
+  article.append(writList)
+
+  const actTitle = document.createElement('h2')
+  actTitle.textContent = 'Actors'
+  article.append(actTitle)
+  const actList = document.createElement('ul')
+  movie.Actors.forEach(function (actor) {
+    const li = document.createElement('li')
+    li.textContent = actor
+    actList.append(li)
+  })
+  article.append(actList)
+
+  const ratings = document.createElement('p')
+  ratings.className = 'ratings'
+  ratings.textContent = 'IMDb: ' + movie.imdbRating + '/10 • Metascore: ' + movie.Metascore
+  article.append(ratings)
+
+  element.append(article)
 }
 
 function loadMovies(genre) {
   const xhr = new XMLHttpRequest();
   xhr.onload = function () {
-    const mainElement = document.querySelector("main");
+    const mainElement = document.querySelector('main');
 
     while (mainElement.childElementCount > 0) {
       mainElement.firstChild.remove()
@@ -56,37 +113,51 @@ function loadMovies(genre) {
         appendMovie(movie, mainElement)
       }
     } else {
-      mainElement.append(`Daten konnten nicht geladen werden, Status ${xhr.status} - ${xhr.statusText}`);
+      mainElement.append('Daten konnten nicht geladen werden, Status ' + xhr.status + ' - ' + xhr.statusText);
     }
   }
 
-  const url = new URL("/movies", location.href)
-  /* Task 1.4. Add query parameter to the url if a genre is given */
+  const url = new URL('/movies', location.href)
+  if (genre) {
+    url.searchParams.set('genre', genre)
+  }
 
-  xhr.open("GET", url)
+  xhr.open('GET', url)
   xhr.send()
 }
 
 window.onload = function () {
   const xhr = new XMLHttpRequest();
   xhr.onload = function () {
-    const listElement = document.querySelector("nav>ul");
+    const listElement = document.querySelector('nav>ul');
 
     if (xhr.status === 200) {
-      /* Task 1.3. Add the genre buttons to the listElement and 
-         initialize them with a click handler that calls the 
-         loadMovies(...) function above. */
-      const genres = JSON.parse(xhr.responseText);
+      const genres = JSON.parse(xhr.responseText)
 
-      /* When a first button exists, we click it to load all movies. */
-      const firstButton = document.querySelector("nav button");
+      const allButton = document.createElement('button')
+      allButton.textContent = 'All'
+      allButton.onclick = function () { loadMovies(null) }
+      const allLi = document.createElement('li')
+      allLi.append(allButton)
+      listElement.append(allLi)
+
+      genres.forEach(function (genre) {
+        const button = document.createElement('button')
+        button.textContent = genre
+        button.onclick = function () { loadMovies(genre) }
+        const li = document.createElement('li')
+        li.append(button)
+        listElement.append(li)
+      })
+
+      const firstButton = document.querySelector('nav button');
       if (firstButton) {
         firstButton.click();
       }
     } else {
-      document.querySelector("body").append(`Daten konnten nicht geladen werden, Status ${xhr.status} - ${xhr.statusText}`);
+      document.querySelector('body').append('Daten konnten nicht geladen werden, Status ' + xhr.status + ' - ' + xhr.statusText);
     }
   };
-  xhr.open("GET", "/genres");
+  xhr.open('GET', '/genres');
   xhr.send();
 };
--- server/server.js
+++ server/server.js
@@ -11,48 +11,59 @@ app.use(bodyParser.json());
 // Serve static content in directory 'files'
 app.use(express.static(path.join(__dirname, 'files')));
 
-/* Task 1.2: Add a GET /genres endpoint:
-   This endpoint returns a sorted array of all the genres of the movies
-   that are currently in the movie model.
-*/
+// Task 1.2: GET /genres — returns sorted array of unique genres from all movies
+app.get('/genres', function (req, res) {
+  const allMovies = Object.values(movieModel);
+  const genreSet = new Set();
 
-/* Task 1.4: Extend the GET /movies endpoint:
-   When a query parameter for a specific genre is given, 
-   return only movies that have the given genre
- */
+  for (const movie of allMovies) {
+    for (const genre of movie.Genres) {
+      genreSet.add(genre);
+    }
+  }
+
+  const sortedGenres = Array.from(genreSet).sort();
+  res.send(sortedGenres);
+});
+
+// Task 2.2: GET /movies — returns all movies, or filtered by genre query parameter
 app.get('/movies', function (req, res) {
-  let movies = Object.values(movieModel)
+  let movies = Object.values(movieModel);
+
+  const genre = req.query.genre;
+  if (genre) {
+    movies = movies.filter(movie => movie.Genres.includes(genre));
+  }
+
   res.send(movies);
-})
+});
 
 // Configure a 'get' endpoint for a specific movie
 app.get('/movies/:imdbID', function (req, res) {
-  const id = req.params.imdbID
-  const exists = id in movieModel
+  const id = req.params.imdbID;
+  const exists = id in movieModel;
  
   if (exists) {
-    res.send(movieModel[id])
+    res.send(movieModel[id]);
   } else {
-    res.sendStatus(404)    
+    res.sendStatus(404);
   }
-})
+});
 
 app.put('/movies/:imdbID', function(req, res) {
-
-  const id = req.params.imdbID
-  const exists = id in movieModel
+  const id = req.params.imdbID;
+  const exists = id in movieModel;
 
   movieModel[req.params.imdbID] = req.body;
   
   if (!exists) {
-    res.status(201)
-    res.send(req.body)
+    res.status(201);
+    res.send(req.body);
   } else {
-    res.sendStatus(200)
+    res.sendStatus(200);
   }
-  
-})
+});
 
-app.listen(3000)
+app.listen(3000);
 
-console.log("Server now listening on http://localhost:3000/")
+console.log("Server now listening on http://localhost:3000/");
